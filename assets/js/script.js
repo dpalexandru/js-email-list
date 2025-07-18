d30132bb1914fad45360b533ba2c1aea2b84ac8b
dpalexandru <alexpoian91@hotmail.com>
2025-07-18 16:00:46 +0200

    ask 10 random emails and display them in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
+//Recupero elemnti dom
+const emailContainer = document.getElementById('emailContainer')
+
 //Ciclo for per genearare 10 email
 for (i = 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-        console.log(resp.data.response);
+        const email = resp.data.response;
+        emailContainer.innerHTML += `<li>${email}</li>`;
     })
 }
