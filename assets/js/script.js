590a95eb626441f36cae8a7cf30ed7c787de3508
dpalexandru <alexpoian91@hotmail.com>
2025-07-18 16:09:51 +0200

    feat: create a button to call the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 //Recupero elemnti dom
 const emailContainer = document.getElementById('emailContainer')
+const genera = document.getElementById('genera');
 
-//Ciclo for per genearare 10 email
-for (i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
-        const email = resp.data.response;
-        emailContainer.innerHTML += `<li>${email}</li>`;
-    })
-}
+//Funzione con Ciclo for per svuotare genearare 10 email
+const generateEmails = () => {
+    emailContainer.innerHTML = '';
+
+    for (i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+            const email = resp.data.response;
+            emailContainer.innerHTML += `<li>${email}</li>`;
+        })
+    }
+};
+//CHiamo la funzione quando si carica la pagina
+generateEmails();
+
+//Button che chiama la funzione di nuovo
+
+genera.addEventListener('click', generateEmails)
